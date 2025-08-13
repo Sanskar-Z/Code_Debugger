93747b0f74b51ca2f2b1a94ab03914deb54e2e1e
Sanskar-Z <sanskarzine21@gmail.com>
2025-08-13 15:46:52 +0530

    ..

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,24 @@
-import { FaFireFlameCurved } from "react-icons/fa6";    
 import { NavLink } from "react-router-dom";
+import { FaCircleUser } from "react-icons/fa6";
+import { FaCode } from "react-icons/fa";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between border-b border-[#f0f2f5] px-10 py-3">
-      <div className="flex items-center gap-4 text-[#111418]">
-        <div className="w-4 h-4 font-bold">
-            <FaFireFlameCurved />
+      <div className="flex flex-1 gap-7">
+        <div className="w-4 h-4">
+            <FaCode size={30}/>
         </div>
         <h2 className="text-lg font-bold tracking-tight">Code Debugger</h2>
       </div>
-      <div className="flex flex-1 justify-end gap-8">
+      <div className="flex flex-1 justify-end gap-8 items-center p-0.5">
         <nav className="flex items-center gap-9">
             <NavLink to="/" className="text-sm font-medium text-[#111418]  hover:bg-gray-100 rounded-xl p-2 transition active:bg-gray-200">Home</NavLink>
             <NavLink to="/features" className="text-sm font-medium text-[#111418] hover:bg-gray-100 rounded-xl p-2 transition active:bg-gray-200">Features</NavLink>
             <NavLink to="/Debugger" className="text-sm font-medium text-[#111418] hover:bg-gray-100 rounded-xl p-2 transition active:bg-gray-200">Debugger</NavLink>
             <NavLink to="/support" className="text-sm font-medium text-[#111418] hover:bg-gray-100 rounded-xl p-2 transition active:bg-gray-200">Support</NavLink> 
         </nav>
-        <button className="h-10 px-4 rounded-lg bg-[#258df4] text-white text-sm font-bold hover:-translate-y-1 hover:scale-105 hover:cursor-pointer transition-transform">
-          Get Started
-        </button>
+          <FaCircleUser size={30} color="#d3d3d3"/>
       </div>
     </header>
   )
--- frontend/src/components/OutputConsole.jsx
+++ frontend/src/components/OutputConsole.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { MdLightMode, MdDarkMode } from 'react-icons/md';
 import { themeContext, counterContext, codeContext } from "../context/context";
+import { GrPowerReset } from "react-icons/gr";
 
 const OutputConsole = () => {
     const [theme, setTheme] = useContext(themeContext);
@@ -21,39 +22,49 @@ const OutputConsole = () => {
             const text = await res.text();
             console.log(text)
             setOutput(text);
-    
+
         } catch (err) {
             console.error("Error running code:", err);
         }
     }
 
+    useEffect(() => {
+        setOutput(null)
+
+    }, [language])
+
+
     return (
         <>
             {/* Buttons */}
-            {/* <h6 className='pt-2  p-1 rounded-xl text-[95%] text-gray-500'>Output</h6> */}
-            <div className="flex justify-end gap-6 my-2 items-center">
-                <button onClick={runCode} className="h-10 px-4 rounded-lg bg-[#1cd71a] active:bg-green-500 text-white text-sm font-bold transform hover:-translate-y-1 hover:scale-105 hover:cursor-pointer transition">
-                    Run Code
-                </button>
-                <button
-                    onClick={toggleTheme}
-                    className="h-10 px-4 rounded-lg bg-[#258df4] hover:bg-blue-500 text-white text-sm font-bold transform hover:-translate-y-1 hover:scale-105 hover:cursor-pointer transition flex items-center gap-1"
-                >
-                    {theme ? (
-                        <>
-                            <MdLightMode /> Light
-                        </>
-                    ) : (
-                        <>
-                            <MdDarkMode /> Dark
-                        </>
-                    )}
-                </button>
+            <div className="flex justify-between gap-6 my-2 items-center">
+                <h6 className='pt-2  p-1 rounded-xl text-[95%] text-gray-500'>Output:</h6>
+                <div className='flex gap-6 items-center'>
+                    <button className={'bg-[#e7e7e7] p-2 rounded-2xl transform hover:-translate-y-0.5 hover:scale-105 hover:cursor-pointer transition'}>
+                        <GrPowerReset size={25} />
+                    </button>
+                    <button onClick={runCode} className="h-10 px-4 rounded-lg bg-[#1cd71a] active:bg-green-500 text-white text-sm font-bold transform hover:-translate-y-1 hover:scale-105 hover:cursor-pointer transition">
+                        Run Code
+                    </button>
+                    <button
+                        onClick={toggleTheme}
+                        className="h-10 px-4 rounded-lg bg-[#258df4] hover:bg-blue-500 text-white text-sm font-bold transform hover:-translate-y-1 hover:scale-105 hover:cursor-pointer transition flex items-center gap-1">
+                        {theme ? (
+                            <>
+                                <MdLightMode /> Light
+                            </>
+                        ) : (
+                            <>
+                                <MdDarkMode /> Dark
+                            </>
+                        )}
+                    </button>
+                </div>
             </div>
 
             {/* Output Console */}
-            <div className='h-[80vh] w-[100%] border-1 my-5 py-2.5 px-1.5 rounded-xl'>
-                {output? output:`Click on "Run Code" to see the output here`}
+            <div className={'h-[80vh] w-[100%] border-1 my-5 py-2.5 px-2.5 rounded-xl' + (theme ? " " : " bg-[#1e1d1d] text-white")}>
+                {output ? output : <p className='text-gray-400'>Click on "Run Code" to see the output here</p>}
             </div>
         </>
     );
